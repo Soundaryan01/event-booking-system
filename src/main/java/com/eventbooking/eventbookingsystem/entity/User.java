6cc815136e80caeb343f461dbedfa5a32da36d2c
Soundaryan01 <soundaryanram@gmail.com>
2026-04-15 20:15:36 +0530

    Added empty entity base structure

---
+++ src/main/java/com/eventbooking/eventbookingsystem/entity/User.java
@@ -0,0 +1,18 @@
+package com.eventbooking.eventbookingsystem.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+    @Id
+    @GeneratedValue
+    private Integer id;
+}
